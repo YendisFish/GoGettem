a151e7dc7c74caa3fe7c9505d8e9933ab45def97
YendisFish <brintonsid@gmail.com>
2021-11-09 16:38:22 -0800

    Added Logging Ability

---
+++ GoGettem/DocumentingHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace GoGettem
+{
+    public class DocumentingHandler
+    {
+        public void AddReference(string SessionToken, string[] AdditionalInformation)
+        {
+            Console.WriteLine("Writing User Info...");
+
+            if (!File.Exists("log.txt"))
+            {
+                var fs = File.Create("log.txt");
+                fs.Close();
+            }
+            else
+            {
+                string ContentToWrite = SessionToken + " | " + AdditionalInformation.ToString(); 
+                File.AppendAllText("log.txt", ContentToWrite);
+            }
+        }
+    }
+}
